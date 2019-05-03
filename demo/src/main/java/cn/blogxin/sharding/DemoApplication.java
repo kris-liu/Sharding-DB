b227d5ee83f33ad92542acaf9ad62925b09731a1
kris <895195774@qq.com>
2019-05-03 21:57:22 +0800

    init

---
+++ demo/src/main/java/cn/blogxin/sharding/DemoApplication.java
@@ -0,0 +1,13 @@
+package cn.blogxin.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
