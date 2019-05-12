6bc9f76ca95dc7f2d2534bb87ad92031d3585115
kris <895195774@qq.com>
2019-05-13 00:21:04 +0800

    添加分库功能

--- demo/src/main/java/cn/blogxin/sharding/plugin/strategy/database/ShardingDataBaseStrategy.java
+++ demo/src/main/java/cn/blogxin/sharding/plugin/strategy/database/ShardingDataBaseStrategy.java
@@ -8,11 +8,12 @@ package cn.blogxin.sharding.plugin.strategy.database;
 public interface ShardingDataBaseStrategy {
 
     /**
-     * 计算获取对应分库key
+     * 计算获取对应分库序号
      *
      * @param sharingDataBaseCount    分库数量
      * @param sharingTableCount       分表数量
      * @param currentShardingTableKey 当前分表位
+     * @return 分库序号
      */
     Integer calculate(int sharingDataBaseCount, int sharingTableCount, int currentShardingTableKey);
 }
