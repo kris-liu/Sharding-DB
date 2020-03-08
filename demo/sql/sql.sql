06c68d850749f0239a20ed3f87652996a8113c8c
kris <895195774@qq.com>
2020-03-08 16:08:56 +0800

    init sql

--- demo/sql/sql.sql
+++ demo/sql/sql.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE `test` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+use test;
+
 create table order_info_0 (
    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
    `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
@@ -43,52 +46,6 @@ create table order_info_4 (
    UNIQUE KEY `uk_order_id` (`order_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
 
-create table order_info_5 (
-   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
-   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
-   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uk_order_id` (`order_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
-
-create table order_info_6 (
-   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
-   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
-   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uk_order_id` (`order_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
-
-create table order_info_7 (
-   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
-   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
-   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uk_order_id` (`order_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
-
-create table order_info_8 (
-   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
-   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
-   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uk_order_id` (`order_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
-
-create table order_info_9 (
-   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
-   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
-   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uk_order_id` (`order_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
-
-
 create table order_ext_0 (
    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
    `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
@@ -133,6 +90,57 @@ create table order_ext_4 (
    PRIMARY KEY (`id`),
    UNIQUE KEY `uk_order_id` (`order_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单扩展表';
+
+
+
+
+CREATE DATABASE `test1` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+use test1;
+
+create table order_info_5 (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
+   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
+   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uk_order_id` (`order_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+create table order_info_6 (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
+   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
+   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uk_order_id` (`order_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+create table order_info_7 (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
+   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
+   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uk_order_id` (`order_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+create table order_info_8 (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
+   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
+   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uk_order_id` (`order_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+create table order_info_9 (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+   `order_id` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '订单号',
+   `status` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态',
+   `remark` varchar(2048) NOT NULL DEFAULT '' COMMENT '备注',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uk_order_id` (`order_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
 
 create table order_ext_5 (
    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
